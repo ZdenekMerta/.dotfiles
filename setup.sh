701a9fd230fcda6299daa3e6d15e275126788e54
Zdenek Merta <iam@zdenekmerta.com>
2023-02-18 16:29:22 +0100

    Install Homebrew

--- setup.sh
+++ setup.sh
@@ -22,7 +22,27 @@ install_xcode() {
   fi
 }
 
+install_homebrew() {
+  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+  if hash brew &>/dev/null; then
+    warn "Homebrew already installed"
+  else
+    info "Installing Homebrew"
+
+    # Reset `sudo` timeout to use Homebrew install in noninteractive mode
+    sudo --validate
+
+    # Install Homebrew
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Turn off Homebrew analytics
+    brew analytics off
+  fi
+}
+
 info "####### dotfiles #######"
 read -p "Press enter to start:"
 
 install_xcode
+install_homebrew
